166c92a076dd45dc29bf81f32e4829690e30d48f
Frank <frankrod44@gmail.com>
2012-05-14 07:20:48 -0400

    updated VM

---
+++ Compiler/VM.cpp
@@ -0,0 +1,238 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <map>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <sstream>
+#include "tools.h"
+
+using namespace std;
+
+const bool DEBUG = true;
+
+ifstream fin;
+
+stack< Object > stk; //object stack
+map< string, Object > memory; //where all variables are stored
+map< void*, int > pointerCt; //naive garbage collector (counts how many variables are pointing to that object)
+map<string, streamoff> jumpLoc; //locations of each label in the source file
+
+void handlePush(Tokenizer tk)
+{
+	string s = tk.nextToken();
+
+	if (isValidIntConst(s)) //pushing an literal int
+	{
+		int * ptr = new int;
+		*ptr = toNum(s);
+
+		stk.push(Object(INTEGER, ptr));
+	}
+	else if (isValidVariableName(s)) //pushing a variable
+	{
+		if (memory.find(s) == memory.end())
+			error("Unidentified variable name :: " + s);
+
+		stk.push(memory[s]);
+	}
+}
+
+void handleLoad(Tokenizer tk)
+{
+	string name = tk.nextToken();
+
+	if (stk.empty())
+		error("Can't load with an empty stack - " + name);
+
+	if (memory.find(name) != memory.end()) //if this variable already exists (recycle it)
+	{
+		void* old = memory[name].ptr;
+		if (pointerCt[old] > 0)
+		{
+			pointerCt[ old ]--;
+			if (pointerCt[ old ] == 0)
+			{	
+				pointerCt.erase(old);
+				delete old;
+			}
+		}
+	}
+
+	memory[name] = stk.top();
+	pointerCt[ stk.top().ptr ]++;
+}
+
+void handlePop()
+{
+	if (stk.empty())
+		error("Can't pop an empty stack!");
+	else 
+	{
+		void * ptr = stk.top().ptr;
+		if (ptr != NULL && pointerCt[ptr] == 0) //if not NULL and no other variables are pointing to it, free it
+		{
+			pointerCt.erase(ptr);
+			delete ptr;
+		}
+
+		stk.pop();
+	}
+}
+
+void handleRead(Tokenizer tk)
+{
+	string type = tk.nextToken();
+
+	if (type == "INT")
+	{
+		int * ptr = new int;
+		cin >> *ptr;
+		stk.push(Object(INTEGER, ptr));
+	}
+	else error("Read does not support that type");
+}
+
+void handleOperation(int op)
+{
+	if (stk.size() < 2) error("Not enough elements in the stack to perform operation " + toStr(op));
+	Object b = stk.top(); stk.pop();
+	Object a = stk.top(); stk.pop();
+
+	if (a.type != b.type) error("operation requires two objects of the same type\n");
+	if (a.ptr == NULL || b.ptr == NULL) error("Null reference while performing operation");
+	
+	if (a.type == INTEGER)
+	{
+		int * pval = new int;
+
+		if (op == ADD)
+			*pval = *(int*)a.ptr + *(int*)b.ptr;
+		else if (op == SUB)
+			*pval = *(int*)a.ptr - *(int*)b.ptr;
+		else if (op == MULT)
+			*pval = *(int*)a.ptr * *(int*)b.ptr;
+		else if (op == DIV)
+		{
+			if (*(int*)b.ptr == 0) error("Division by ZERO");
+			*pval = *(int*)a.ptr / *(int*)b.ptr;
+		}
+		else if (op == MOD)
+		{
+			if (*(int*)b.ptr == 0) error("Modulo by ZERO");
+			*pval = *(int*)a.ptr % *(int*)b.ptr;
+		}
+		else if (op == LESS_THAN)
+			*pval = *(int*)a.ptr < *(int*)b.ptr;
+		else if (op == LESS_THAN_OR_EQUAL)
+			*pval = *(int*)a.ptr <= *(int*)b.ptr;
+		else if (op == EQUAL)
+			*pval = *(int*)a.ptr == *(int*)b.ptr;
+		else if (op == GREATER_THAN_OR_EQUAL)
+			*pval = *(int*)a.ptr >= *(int*)b.ptr;
+		else if (op == GREATER_THAN)
+			*pval = *(int*)a.ptr > *(int*)b.ptr;
+		else error("int type does not support this operation");
+
+		if (pointerCt[a.ptr] == 0) delete a.ptr;
+		if (pointerCt[b.ptr] == 0) delete b.ptr;
+
+		stk.push(Object(INTEGER, pval));
+	}
+	else error("Unidentified type while performing an operation" + toStr(a.type));
+}
+
+void handlePrint(Tokenizer tk)
+{
+	string s = tk.nextToken();
+
+	if (s == "") //print top of stack
+		cout << stk.top().valToStr() << endl;
+	else if (isValidVariableName(s))
+	{
+		if (memory.find(s) == memory.end())
+			error("Cannot find variable named - " + s);
+
+		cout << memory[s].valToStr() << endl;
+	}
+	else if (isValidIntConst(s))
+		cout << s << endl;
+	else error("Cannot print - " + s);
+}
+
+void printTrace()
+{
+	cerr << "Done\n\nContents of Memory\n-------------------------\n";
+
+	for (map<string, Object>::iterator it = memory.begin(); it!=memory.end(); it++)
+		cerr << it->first << " = " << (it->second).valToStr() << endl;
+	
+	cerr << "-------------------------\n";
+
+	cerr << "\nContents of Stack\n-------------------------\n";
+	while (!stk.empty())
+	{
+		cerr << stk.top().type << " - " << stk.top().valToStr() << endl;
+		stk.pop();
+	}
+	cerr << "-------------------------\n";
+
+	cerr << "\nContents of Pointer Count\n-------------------------\n";
+	for (map<void*, int>::iterator it = pointerCt.begin(); it!=pointerCt.end(); it++)
+		cerr << it->first << " = " << it->second  << " == " << *(int*)it->first << endl;
+
+	cerr << "-------------------------\n";
+}
+
+int main (int argc, char *argv[])
+{
+	if (argc == 1) fin = ifstream("inst.txt");
+	else if (argc == 2) fin = ifstream(argv[1]);
+	else error("Invalid console arguments");
+
+	string str;
+	fin >> str;
+	
+	if (str != ".START") 
+		error("Code should start with .START");
+
+	getline(fin, str); //update cursor
+
+	while (true)
+	{
+		if (fin.eof()) 
+			error("no '.END' found");
+		
+		getline(fin, str);
+		Tokenizer tk(str);
+		string cmd = tk.nextToken();
+
+		if (DEBUG) cerr << "At :: " << cmd << endl;
+
+		if (cmd == "") continue; //blank line, whoops
+		else if (cmd == ".END") break; 
+		else if (cmd == "PUSH") handlePush(tk);
+		else if (cmd == "LOAD") handleLoad(tk);
+		else if (cmd == "POP") handlePop();
+		else if (cmd == "READ") handleRead(tk);
+		else if (cmd == "ADD") handleOperation(ADD);
+		else if (cmd == "SUB") handleOperation(SUB);
+		else if (cmd == "MULT") handleOperation(MULT);
+		else if (cmd == "DIV") handleOperation(DIV);
+		else if (cmd == "MOD") handleOperation(MOD);
+		else if (cmd == "OP<") handleOperation(LESS_THAN);
+		else if (cmd == "OP<=") handleOperation(LESS_THAN_OR_EQUAL);
+		else if (cmd == "OP=") handleOperation(EQUAL);
+		else if (cmd == "OP>=") handleOperation(GREATER_THAN_OR_EQUAL);
+		else if (cmd == "OP>") handleOperation(GREATER_THAN);
+		else if (cmd == "PRINT") handlePrint(tk);
+		else error("Do not recognize cmd - " + cmd);
+	}
+
+	if (DEBUG) printTrace();
+	
+	return 0;
+}
---
+++ Compiler/tools.h
@@ -0,0 +1,248 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <cstdlib>
+#include <cstdio>
+#include <map>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <sstream>
+using namespace std;
+
+//All of the types that I currently support
+enum Types { INTEGER };
+enum Operations { ADD, SUB, MULT, DIV, MOD, LESS_THAN, LESS_THAN_OR_EQUAL, EQUAL, GREATER_THAN_OR_EQUAL, GREATER_THAN};
+
+void error(string s)
+{
+	cerr << "ERROR :: " << s << endl;
+	exit(0);
+}
+
+struct Object
+{
+	int type; //type of object
+	void * ptr; //location in memory
+	
+	Object() {}
+	Object(int a, void * b)
+	{ type = a; ptr = b; }
+
+	/*~Object()
+	{ 
+		if (ptr != NULL)
+			delete ptr; 
+	}*/
+
+	string valToStr()
+	{
+		stringstream ss;
+		if (type == INTEGER)
+			ss << *(int*)ptr;
+		else error("Unidentified Object type, cannot print it.");
+
+		return ss.str();
+	}
+};
+
+bool isValidIntConst(string a) //checks if the string represents a valid literal int
+{
+	int inx = 0;
+	if (a[0] == '-') inx++;
+
+	for (int i=inx; i<(int)a.length(); i++)
+		if (!isdigit(a[i])) return false;
+
+	return true;
+}
+
+bool isValidVariableName(string a) //valid regex = [_A-Za-z][_0-9A-Za-z]*
+{
+	bool ok = a.length()>0 && (a[0]=='_' || isalpha(a[0]));
+	for (int i=1; i<(int)a.length() && ok; i++)
+		ok &= isalpha(a[i]) || isdigit(a[i]) || a[i]=='_';
+
+	return ok;
+}
+
+int toNum(string s) //string -> integer
+{
+	stringstream ss(s);
+	int ret;
+	ss >> ret;
+	return ret;
+}
+
+template<class T>
+string toStr(T n)
+{
+	stringstream ss;
+	ss << n;
+	return ss.str();
+}
+
+bool isOperator(char a)
+{
+	return (a=='+' ||
+			a=='-' ||
+			a=='*' ||
+			a=='/' ||
+			a=='%' ||
+			a=='<' ||
+			a=='=' ||
+			a=='>' ||
+			a=='$');
+}
+
+struct Tokenizer
+{
+	string str;
+	int inx;
+
+	Tokenizer(string s) { str = s; inx=0; }
+
+	void update()
+	{
+		while (inx<(int)str.length() && isspace(str[inx])) inx++; //next non-whitespace character
+	}
+
+	bool hasGroup()
+	{ 
+		update();
+		return str[inx] == '(';
+	}
+
+	string nextGroup()
+	{
+		update(); //sanity check
+		
+		string ret = "";
+		int c = 0;
+		do
+		{
+			if (str[inx] == '(') c++;
+			else if (str[inx] == ')') c--;
+			
+			ret += str[inx];
+			inx++;
+		}
+		while (inx < (int)str.length() && c!=0);
+
+		return ret.substr(1, ret.length()-2)+"$";
+	}
+
+	int nextInt()
+	{
+		update();
+		int d = 0;
+		if (isdigit(str[inx]) || str[inx] == '-')
+		{
+			int f = 1; //controls the sign
+			if (str[inx] == '-') 
+			{
+				f = -1;
+				inx++;
+			}
+
+			d = 0;
+			while (isdigit(str[inx]))
+				d = 10*d + (str[inx++]-'0');
+
+			return f*d;
+		}
+		else error("Integer expected, instead found :: " + str[inx]); 
+
+		return -666; //should never get here
+	}
+
+	string nextOp()
+	{
+		update();
+
+		if (isOperator(str[inx]))
+		{
+			if ((str[inx] == '<' || str[inx] == '>') && str[inx+1]=='=')
+			{
+				inx += 2;
+				return str.substr(inx-2,2);
+			}
+			else return string(1, str[inx++]);
+		}
+		else
+		{
+			cout << "ERROR :: Operator expected, found '" << str[inx] << "' instead\n";
+			exit(0);
+		}
+
+		return "should never get here";
+	}
+
+	bool hasNext()
+	{
+		update();
+		return inx != str.length();
+	}
+
+	string nextToken()
+	{
+		update();
+		string ret = "";
+		while (inx < (int)str.length() && !isspace(str[inx]))
+			ret += str[inx++];
+
+		return ret;
+	}
+
+	string getAll() //get whatever is left to read
+	{
+		update();
+		return str.substr(inx);
+	}
+
+	bool hasNextInt()
+	{
+		return (isdigit(str[inx]) || str[inx] == '-');
+	}
+
+	string nextVar()
+	{
+		update();
+
+		string name = "";
+		while (inx < (int)str.length()-1 && !isspace(str[inx]) && !isOperator(str[inx]))
+			name += str[inx++];
+
+		return name;
+	}
+
+	bool isEmpty()
+	{
+		update();
+		return inx == str.length();
+	}
+
+	char peek()
+	{
+		update();
+		return str[inx];
+	}
+
+	string nextLiteralString()
+	{
+		string ret = "";
+		if (peek() != '"')
+		{
+			cout << "ERROR :: String literal must beging with '\"', not ' " << peek() << "'\n";
+			exit(0);
+		}
+
+		inx++;
+		while (inx<(int)str.length() && !(str[inx-1] != '\\' && str[inx] == '"') )
+			ret += str[inx++];
+
+		inx++; //eat last quote
+		return ret;
+	}
+};
+
